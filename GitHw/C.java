cdf3ac61d1db1feb424caf4d1f8d3bef075c487f
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2025-12-14 21:02:08 +0900

    Alice: 장바구니 기능 및 지문 인식 추가

--- GitHw/C.java
+++ GitHw/C.java
@@ -3,5 +3,6 @@ package GitHw;
 public class C {
     public void login() {
         System.out.println("[인증] 학번과 비밀번호로 로그인합니다.");
+        System.out.println("[인증] 지문 인식(TouchID) 로그인."); // Alice
     }
 }
