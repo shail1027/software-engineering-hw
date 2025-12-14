3c4197ff92da28d83d4af4c839681446a139584f
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2025-12-14 21:02:48 +0900

    Master: 2단계 인증 적용

--- GitHw/C.java
+++ GitHw/C.java
@@ -2,6 +2,6 @@ package GitHw;
 
 public class C {
     public void login() {
-        System.out.println("[인증] 학번과 비밀번호로 로그인합니다.");
+        System.out.println("[인증] 2단계 인증(OTP) 로그인."); // Master
     }
 }
